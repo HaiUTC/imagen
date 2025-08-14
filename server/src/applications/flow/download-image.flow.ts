c243c0d90593d549cd5f8498c6bf6de0537e3cf5
Nguyễn Thanh Hải <sahaetran@gmail.com>
2025-08-14 15:58:21 +0700

    update: download batch

--- server/src/applications/flow/download-image.flow.ts
+++ server/src/applications/flow/download-image.flow.ts
@@ -4,12 +4,19 @@ export const downloadImageGeneratedFlow = async (option: string, id: string) => 
   let images: string[] = [];
 
   if (option === 'all') {
-    images = await Promise.all([
+    // Batch 1: Download images 1 and 2 concurrently
+    const [image1, image2] = await Promise.all([
       imagenService.downloadImageGenerated(id, 1, process.env.VISION_DOWLOAD_API_KEY1!),
       imagenService.downloadImageGenerated(id, 2, process.env.VISION_DOWLOAD_API_KEY2!),
+    ]);
+    images.push(image1, image2);
+
+    // Batch 2: Download images 3 and 4 concurrently
+    const [image3, image4] = await Promise.all([
       imagenService.downloadImageGenerated(id, 3, process.env.VISION_DOWLOAD_API_KEY3!),
       imagenService.downloadImageGenerated(id, 4, process.env.VISION_DOWLOAD_API_KEY4!),
     ]);
+    images.push(image3, image4);
   } else {
     const image = await imagenService.downloadImageGenerated(id, +option, process.env.VISION_DOWLOAD_API_KEY1!);
     images.push(image);
